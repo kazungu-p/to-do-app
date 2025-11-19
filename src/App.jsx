c9d6bd517083ec8eb596c525b9201621cd81e1f4
kazungu-p <patrickkazungu@icloud.com>
2025-11-19 23:05:25 +0300

    Add Styling and functionaality - Can add items

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./assets/App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let [addedText, setAddedText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const newText = event.target.value;
+    setAddedText(newText);
+  }
+  function addItem(event) {
+    setItems(prevValue => {
+      return [addedText, ...items]
+    })
+
+    setAddedText("");
+    event.preventDefault();
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="heading">
+        <h1>To-do</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <form>
+        <input onChange={handleChange} type="text" value={addedText} />
+        <button onClick={addItem}>
+          Add
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </form>
+      <ul>
+        {items.map(item => {
+          return <li>{item}</li>
+        })}
+      </ul>
+    </div>
   )
 }
 
